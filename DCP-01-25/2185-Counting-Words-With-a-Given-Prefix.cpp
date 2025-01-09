2036e616f7dd7362ecae9b2641d5214f6b29a52c
shady <149704119+shady-2004@users.noreply.github.com>
2025-01-09 03:05:59 +0200

    [2185] [Time Beats: 100.00%] [Memory Beats: 95.47%] - LeetPush

---
+++ DCP-01-25/2185-Counting-Words-With-a-Given-Prefix.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int c=0;
+        for(int i=0;i<words.size();i++){
+            if(words[i].find(pref)==0)c++;
+        }
+        return c;
+    }
+};
