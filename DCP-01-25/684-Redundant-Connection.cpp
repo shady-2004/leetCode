251500f6ec8ce881405b3c4a589784f60b0d8ce9
shady <149704119+shady-2004@users.noreply.github.com>
2025-01-29 13:38:06 +0200

    [684] [Time Beats: 100.00%] [Memory Beats: 69.21%] - LeetPush

--- DCP-01-25/684-Redundant-Connection.cpp
+++ DCP-01-25/684-Redundant-Connection.cpp
@@ -1,40 +1,36 @@
 class Solution {
-    private:
-    int find(unordered_map<int,int>&par,int x){
-        int p=par[x];
-        while(p!=par[par[p]])
-            p=par[par[p]];
-        return p;
-    }
-    bool unite(unordered_map<int,int>&par,unordered_map<int,int>&rank,int n1,int n2){
-        int p1=find(par,n1),p2=find(par,n2);
-        if(p1==p2)return false;
-        if(rank[p1]>rank[p2])
-            par[p2]=p1;
-        else if(rank[p1]<rank[p2])
-            par[p1]=p2;
-
+    bool onion(int x,int y,vector<int>&parent, vector<int>&rank){
+        if(dad(x,parent)==dad(y,parent)) return false;
+        int parX=dad(x,parent);
+        int parY=dad(y,parent);
+        if(rank[parX]>rank[parY]){
+            parent[parY]=parX;
+            rank[parX]+=rank[parY];
+        }
         else{
-            par[p2]=p1;
-            rank[p1]++;
+             parent[parX]=parY;
+            rank[parY]+=rank[parX];
         }
         return true;
     }
+    int dad(int x,vector<int>&parent){
+        if(parent[x]==x)return x;
+        parent[x]=dad(parent[x],parent);
+        return parent[x];
+    }
+
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        unordered_map<int,int>par;
-        unordered_map<int,int>rank;
-        for(int i=1;i<=edges.size();i++){
-            par[i]=i;
-            rank[i]=0;
+      vector<int>parent(edges.size()+1);
+      vector<int>rank(edges.size()+1,0);
+             for(int i=1;i<edges.size()+1;i++)
+                 parent[i]=i;
+    
+        for(auto&edge:edges){
+            if(!onion(edge[0],edge[1],parent,rank))
+                return {edge[0],edge[1]};
         }
-        int x,y;
-        for(auto a:edges){
-           if(!unite(par,rank,a[0],a[1])) {
-            x=a[0];
-            y=a[1];
-           } 
-        }
-        return {x,y};
+
+        return {-1,-1};
     }
 };
