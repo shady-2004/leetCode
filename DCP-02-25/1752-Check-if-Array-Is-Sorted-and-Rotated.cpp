e321bb95d5b527917d40366e07792f6b50b0ac5b
shady <149704119+shady-2004@users.noreply.github.com>
2025-02-02 17:25:21 +0200

    [1752] [Time Beats: 100.00%] [Memory Beats: 85.59%] - LeetPush

--- DCP-02-25/1752-Check-if-Array-Is-Sorted-and-Rotated.cpp
+++ DCP-02-25/1752-Check-if-Array-Is-Sorted-and-Rotated.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
     int n=nums.size();
-    for(int i=0;i<n*2;i++){
-        int j=i;
-        while(j<n*2-1&&nums[j%n]<=nums[(j+1)%n])j++;
-        if(j-i+1==n)return true;
+    int cnt=0;
+    for(int i=1;i<n*2;i++){
+       if(nums[i%n]<nums[(i-1)%n])cnt++;
     }
-    return false;
+    return cnt<=2;
 
     }
 };
