306c40e3adcb042ee6b4982da53238f8ba9b82ab
shady <149704119+shady-2004@users.noreply.github.com>
2025-02-05 17:48:29 +0200

    [1790] [Time Beats: 100.00%] [Memory Beats: 61.87%] - LeetPush

--- DCP-02-25/1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.cpp
+++ DCP-02-25/1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
         int diff=0;
-       vector<int>freq1(26,0);
-       vector<int>freq2(26,0);
+       vector<int>freq(26,0);
 
         for(int i=0;i<s2.size();i++){
             if(s1[i]!=s2[i])diff++;
-            freq1[s1[i]-'a']++;
-            freq2[s2[i]-'a']++;
+            freq[s1[i]-'a']++;
+            freq[s2[i]-'a']--;
         }
         
-        for(int i=0;i<26;i++)if(freq1[i]!=freq2[i]) return false;
+        for(int i=0;i<26;i++)if(freq[i]!=0) return false;
         return (diff==2||diff==0);
 
 
