514465cc235339442bd0fe4e6e327bf804884d05
shady <149704119+shady-2004@users.noreply.github.com>
2025-02-06 22:01:27 +0200

    [1726] [Time Beats: 56.97%] [Memory Beats: 68.11%] - LeetPush

--- DCP-02-25/1726-Tuple-with-Same-Product.cpp
+++ DCP-02-25/1726-Tuple-with-Same-Product.cpp
@@ -5,13 +5,12 @@ public:
         int count=0;
         unordered_map<int,int>freq;
         for(int i=0;i<nums.size();i++)
-            for(int j=0;j<nums.size();j++){
-                if(j==i)continue;
+            for(int j=i+1;j<nums.size();j++){
                 freq[nums[i]*nums[j]]++;
             }
 
         for(auto&[_,c]:freq){
-            if(c>=4)count+=(c-2)*c;
+            count+=c>=2?(2*c-2)*2*c:0;
         }
             return count;
     }
