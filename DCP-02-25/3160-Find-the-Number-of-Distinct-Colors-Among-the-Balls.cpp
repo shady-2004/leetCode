a252a39d046c4856d2ad46a98d66e69f78aae6b2
shady <149704119+shady-2004@users.noreply.github.com>
2025-02-07 18:16:02 +0200

    [3160] [Time Beats: 90.35%] [Memory Beats: 93.89%] - LeetPush

--- DCP-02-25/3160-Find-the-Number-of-Distinct-Colors-Among-the-Balls.cpp
+++ DCP-02-25/3160-Find-the-Number-of-Distinct-Colors-Among-the-Balls.cpp
@@ -4,19 +4,16 @@ public:
         vector<int>res(queries.size());
         unordered_map<int,int>freq;
         unordered_map<int,int>colors;
-        for(int i=0;i<queries.size();i++){
+        for(int i=0;i<queries.size();i++){   
             if(colors.count(queries[i][0])){
                 freq[colors[queries[i][0]]]--;
                 if(freq[colors[queries[i][0]]]==0){
                     freq.erase(colors[queries[i][0]]);
                 }
-                freq[queries[i][1]]++;
-                colors[queries[i][0]]=queries[i][1];
             }
-            else{
-                colors[queries[i][0]]=queries[i][1];
-                freq[queries[i][1]]++;
-            }
+            colors[queries[i][0]]=queries[i][1];
+            
+            freq[queries[i][1]]++;
             res[i]=freq.size();
         }
         return res;
