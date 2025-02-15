d30952b686cce3a478748476a48f9fb85c1ff38a
shady <149704119+shady-2004@users.noreply.github.com>
2025-02-15 21:23:03 +0200

    [2974] [Time Beats: 98.10%] [Memory Beats: 10.80%] - LeetPush

--- 2974-Minimum-Number-Game.java
+++ 2974-Minimum-Number-Game.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] numberGame(int[] nums) {
-        int [] arr=new int [nums.length];
         Arrays.sort(nums);
-        for(int i=nums.length-1;i>=0;i-=2){
-            arr[i]=nums[i-1];
-            arr[i-1]=nums[i];
+        for(int i=0;i<nums.length-1;i+=2){
+           int temp=nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
         }
-        return arr;
+        return nums;
     }
 }
