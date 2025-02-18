181d3a2bce4885f5e4519047d00a2b2284cb145e
shady <149704119+shady-2004@users.noreply.github.com>
2025-02-18 02:53:40 +0200

    [2375] [Time Beats: 25.36%] [Memory Beats: 63.49%] - LeetPush

---
+++ DCP-02-25/2375-Construct-Smallest-Number-From-DI-String.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    void rec(string&pattern,string &cur,string &res ,vector<bool>&visited,int i,int prev=0){
+        if(i==-1){
+            if(res.size())
+            res=res>cur?cur:res;
+            else res=cur;
+            return;
+        }
+        if(pattern[i]=='D'){
+            for(int j=prev+1;j<=9;j++){
+                if(visited[j])continue;
+                cur[i]=j+'0';
+                visited[j]=true;
+                rec(pattern,cur,res,visited,i-1,j);
+                visited[j]=false;
+            }
+        }
+        else{
+           for(int j=prev-1;j>=1;j--){
+                if(visited[j])continue;
+                cur[i]=j+'0';
+                visited[j]=true;
+                rec(pattern,cur,res,visited,i-1,j);
+                visited[j]=false;
+            }
+        }
+    }
+public:
+    string smallestNumber(string pattern) {
+        vector<bool>visited(10,false);
+        string cur;
+        string res;
+        for(int i=0;i<=pattern.size();i++)cur+='0';
+        for(int i=1;i<=9;i++){
+                visited[i]=true;
+                cur[pattern.size()]=i+'0';
+                rec(pattern,cur,res,visited,pattern.size()-1,i);
+                visited[i]=false;
+        }
+        return res;
+    }
+};
