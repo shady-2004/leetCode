ececa871680e9443fbcbfcbab6f56975c831d60a
shady <149704119+shady-2004@users.noreply.github.com>
2025-02-19 02:53:36 +0200

    [1415] [Time Beats: 100.00%] [Memory Beats: 99.21%] - LeetPush

--- DCP-02-25/1415-The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.cpp
+++ DCP-02-25/1415-The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.cpp
@@ -1,18 +1,14 @@
 class Solution {
-    bool rec(string& cur, string& res,int n,int k,int &cnt,int i=0,char prev='z'){
+    bool rec(string& cur,int n,int k,int &cnt,int i=0,char prev='z'){
         if(i==n){
             cnt++;
-            if(cnt==k){
-                res=cur;
-                return true;
-            }
-            return false;
+           return cnt==k;
         }
 
         for(int j=0;j<=2;j++){
             if('a'+j==prev)continue;
             cur+=('a'+j);
-            if(rec(cur,res,n,k,cnt,i+1,'a'+j))return true;
+            if(rec(cur,n,k,cnt,i+1,'a'+j))return true;
             cur.pop_back();
         }
         return false;
@@ -20,10 +16,9 @@ class Solution {
 public:
     string getHappyString(int n, int k) {
         int cnt=0;
-        string res;
         string cur;
-        rec(cur,res,n,k,cnt);
+        rec(cur,n,k,cnt);
         if(cnt<k)return "";
-        return res;
+        return cur;
     }
 };
