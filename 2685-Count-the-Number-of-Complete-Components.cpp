3b8c15d586f4afdea45eac13c2e23be919b62088
shady <149704119+shady-2004@users.noreply.github.com>
2025-02-21 18:50:45 +0200

    [2685] [Time Beats: 100.00%] [Memory Beats: 94.35%] - LeetPush

---
+++ 2685-Count-the-Number-of-Complete-Components.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<int>rows(n,0);
+        vector<int>cols(m,0);
+        
+        unordered_map<int,pair<int,int>>locs;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+               locs[mat[i][j]]=pair<int,int>({i,j});
+
+        for(int i=0;i<arr.size();i++){
+            rows[locs[arr[i]].first]++;
+            cols[locs[arr[i]].second]++;
+            if(rows[locs[arr[i]].first]==m||cols[locs[arr[i]].second]==n)
+                return i;
+        }
+        return -1;    
+
+    }
+};
