44f98bca8b94eec847425b9db4c7b15d9bc2d7e7
shady <149704119+shady-2004@users.noreply.github.com>
2025-03-08 02:18:27 +0200

    [2379] [Time Beats: 100.00%] [Memory Beats: 62.68%] - LeetPush

--- DCP-03-25/Minimum-Recolors-to-Get-K-Consecutive-Black-Blocks.cpp
+++ DCP-03-25/Minimum-Recolors-to-Get-K-Consecutive-Black-Blocks.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
-        int l=0;
         int r=k;
         int c=count(blocks.begin(),blocks.begin()+k,'B');
         int res=k-c;
         while(r<blocks.size()){
-            c-=blocks[l++]=='B';
+            c-=blocks[r-k]=='B';
             c+=blocks[r++]=='B';
             res=min(res,k-c);
         }
