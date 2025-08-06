c3f560dc647cdd320307aee488bc98d7e6c0647f
shady <149704119+shady-2004@users.noreply.github.com>
2025-08-06 15:58:49 +0300

    [705] [Time Beats: 94.19%] [Memory Beats: 57.51%] - LeetPush

---
+++ Design-HashSet.py
@@ -0,0 +1,21 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.data = set()
+
+    def add(self, key: int) -> None:
+        self.data.add(key)
+
+    def remove(self, key: int) -> None:
+        if self.contains(key) :
+            self.data.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.data
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
