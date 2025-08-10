73deb9dfc7780ec147ee8f37eeb1c313c3d04063
shady <149704119+shady-2004@users.noreply.github.com>
2025-08-10 10:48:43 +0300

    [869] [Time Beats: 5.34%] [Memory Beats: 53.05%] - LeetPush

---
+++ DCP-08-25/Reordered-Power-of-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        num = f"{n}"
+        visited = [0 for _ in range(len(num))]
+        def permu(cur):
+            if len(cur) == len(num) :
+                if cur[0] == "0" :
+                    return False
+                if int(cur) & (int(cur)-1) ==0 :
+                    return True
+                return False
+
+            for j in range(len(visited)) :
+                if visited[j] == 0 :
+                    visited[j] = 1
+                    res = permu(cur + num[j])
+                    visited[j] = 0
+                    if res ==True :
+                        return True
+            return False
+        return permu("")
