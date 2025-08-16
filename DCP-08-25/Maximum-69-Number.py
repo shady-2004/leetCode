b742776856adc0d954510dd12758a7bc2c16556f
shady <149704119+shady-2004@users.noreply.github.com>
2025-08-16 03:05:28 +0300

    [1323] [Time Beats: 100.00%] [Memory Beats: 47.05%] - LeetPush

---
+++ DCP-08-25/Maximum-69-Number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        s = f"{num}"
+        for i in range(len(s)) :
+            if s[i] =='6' :
+                s = s[:i]+'9'+s[i+1:]
+                break
+        return int(s)
