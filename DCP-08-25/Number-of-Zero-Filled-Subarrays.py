004e0db62f71771cab3fb4e8f1ab9f81c056d5ff
shady <149704119+shady-2004@users.noreply.github.com>
2025-08-19 10:21:51 +0300

    [2348] [Time Beats: 99.61%] [Memory Beats: 42.33%] - LeetPush

--- DCP-08-25/Number-of-Zero-Filled-Subarrays.py
+++ DCP-08-25/Number-of-Zero-Filled-Subarrays.py
@@ -1,19 +1,14 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        occs = []
         cur = 0
+        res = 0
         for num in nums :
             if num == 0:
                 cur += 1
-            else :
-                if cur > 0:
-                    occs.append(cur)
-                    cur = 0
-        if cur > 0 :
-            occs.append(cur)
+            elif  cur > 0 :
+                res += cur*(cur+1)/2
+                cur = 0
 
-        res = 0
-        print(occs)
-        for i in occs :
-            res += i*(i+1) / 2
+        if cur > 0 :
+            res += cur*(cur+1)/2
         return int(res)
