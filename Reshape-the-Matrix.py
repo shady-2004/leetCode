7f8aafbf21f6c1b7fbde8d0ce4e77a68a983c7f0
shady <149704119+shady-2004@users.noreply.github.com>
2026-06-16 09:52:28 +0300

    [566] [Time Beats: 100.00%] [Memory Beats: 10.39%] - LeetPush

--- Reshape-the-Matrix.py
+++ Reshape-the-Matrix.py
@@ -4,15 +4,11 @@
 4        if n*m != r*c :
 5            return mat
 6        matrix = [[0] * c for _ in range(r)]
-7        x = 0
-8        y = 0
+7
+8        idx = 0
 9        for i in range(n):
 10            for j in range(m):
-11                print (x , y)
-12                if y == c :
-13                    y = 0
-14                    x += 1
-15                matrix[x][y] = mat[i][j]
-16                y += 1
-17
-18        return matrix
+11                matrix[idx//c][idx%c] = mat[i][j]
+12                idx += 1
+13
+14        return matrix
