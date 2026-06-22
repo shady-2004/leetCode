82146134fdebb04f4fef37e95dfd7a20f859f581
shady <149704119+shady-2004@users.noreply.github.com>
2026-06-22 15:10:25 +0300

    [1189] [Time Beats: 61.87%] [Memory Beats: 38.08%] - LeetPush

--- Maximum-Number-of-Balloons.py
+++ Maximum-Number-of-Balloons.py
@@ -1,26 +1,15 @@
-class Solution:
-    def maxNumberOfBalloons(self, text: str) -> int:
-        freq={}
-
-        balloon =  set(char for char in "balloon")
-        for char in text :
-            if char in freq :
-                freq[char] += 1
-            else :
-                freq[char] = 1 
-
-        for char in balloon :
-            if char not in freq :
-                return 0
-
-        freq['l'] = freq['l']//2
-
-        freq['o'] = freq['o']//2
-
-        count = 100000
-
-        for char in text :
-           
-            if char in balloon and count > freq[char] :
-                count = freq[char]
-        return count
+1class Solution:
+2    def maxNumberOfBalloons(self, text: str) -> int:
+3        freq = {"b":1 ,"a":1 , "l":2 , "o":2 , "n":1}
+4        cnt = {"b":0 ,"a":0 , "l":0 , "o":0 , "n":0}
+5        for s in text :
+6            if s in freq :
+7                cnt[s] += 1
+8        
+9        return min(
+10            cnt["b"] // freq["b"],
+11            cnt["a"] // freq["a"],
+12            cnt["l"] // freq["l"],
+13            cnt["o"] // freq["o"],
+14            cnt["n"] // freq["n"],
+15        )
