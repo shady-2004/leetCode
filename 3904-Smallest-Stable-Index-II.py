b921cfb46fabb068e3b6960438ad3b03412ee681
shady <149704119+shady-2004@users.noreply.github.com>
2026-09-05 07:42:24 +0300

    [3904] [Time Beats: 31.37%] [Memory Beats: 92.73%] - LeetPush

---
+++ 3904-Smallest-Stable-Index-II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        score = [0 for _ in range(n)]
+        MAX = nums[0]
+        MIN = nums[n-1]
+        res = -1 
+        for i in range(n) : 
+            MAX = max(MAX,nums[i])
+            score[i] = MAX
+
+        for i in range(n-1,-1,-1) : 
+            MIN = min(MIN,nums[i])
+            score[i] -= MIN 
+            if score[i] <= k  :
+                res = i
+        return res
+                
+        
